6222053bbc7d6df12ec6d680c361c3c6a77f5a2a
yazgoo <yazgoo@gmail.com>
2020-02-12 07:30:04 +0100

    :sparkles: first commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+extern crate regex;
+use std::process::Command;
+use std::os::unix::process::CommandExt;
+use std::error::Error;
+use std::env;
+use regex::Regex;
+
+pub fn play_video(player: &String, path: &String) -> Result<(), Box<dyn Error>> {
+    let exe = env::current_exe()?;
+    let bin_dir = exe.parent().ok_or("bin_dir")?.to_str().ok_or("to str")?;
+    let mut quiet = "-quiet";
+    let mut vo = "-vo";
+    let mut ld_preload = format!("{}/libcaca_blockish.so", bin_dir);
+    let mut com = Command::new(player);
+    if player == "cvlc" {
+        quiet = "--quiet";
+        vo = "-V";
+        com.env("DISPLAY", "");
+        ld_preload += ":";
+        let output = Command::new("ldconfig").arg("-p").output()?;
+        let pattern = Regex::new(r"^[\s]*libcaca.so .* ([^ ]+)$")?;
+        ld_preload += String::from_utf8(output.stdout)?
+            .lines()
+            .filter_map(|line| pattern.captures(line) )
+            .map(|cap| cap[1].to_string())
+            .collect::<Vec<String>>()[0].as_str();
+    }
+    com.env("CACA_DRIVER", "raw")
+        .env("LD_PRELOAD", ld_preload)
+        .arg(quiet)
+        .arg(vo)
+        .arg("caca")
+        .arg(path)
+        .exec();
+    Ok(())
+}
+
